47d72078000c6899958c8f271893206674051fdd
Mo Battah <mohhandb@hotmail.com>
2017-03-29 12:09:40 -0400

    Update core.py

--- core.py
+++ core.py
@@ -5,9 +5,9 @@ import codecs
 import json
 
 # To test against the recent changes, use these 3 lines:
-#finra = 'http://tsp.finra.org/finra_org/ticksizepilot/TSPilotChanges.txt'
-#col_program = 7
-#col_symbol = 1
+# finra = 'http://tsp.finra.org/finra_org/ticksizepilot/TSPilotChanges.txt'
+# col_program = 7
+# col_symbol = 1
 
 # ...and comment these three out. These three use today's current list
 finra = 'http://tsp.finra.org/finra_org/ticksizepilot/TSPilotSecurities.txt'
@@ -15,10 +15,10 @@ col_program = 4
 col_symbol = 0
 
 #
-csvfile = csv.reader(codecs.iterdecode(urllib.urlopen(finra), 'utf-8'),  delimiter='|')
+csvfile = csv.reader(codecs.iterdecode(urllib.urlopen(finra), 'utf-8'), delimiter='|')
 prog = re.compile('G[23]')
 
-x = [] # Temp hold for symbols
+x = []  # Temp hold for symbols
 
 # Header/footer don't matter
 for line in csvfile:
@@ -26,14 +26,15 @@ for line in csvfile:
         x.append(line[col_symbol])
 
 # RobinHood API
-rh = urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + ",".join(map(str, x)).replace(' ','+'))
+rh = urllib.urlopen(
+    'https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + ",".join(map(str, x)).replace(' ', '+'))
 
-#Below line works. Now onto creating a line which does this automatically.
-#str_rh = urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=FCEL').read().decode('UTF-8')
+# Below line works. Now onto creating a line which does this automatically.
 
-str_rh = urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + ",".join(map(str, x)).replace(' ', '+'))
-
+str_rh = urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=NIHD,FCEL,NAO').read().decode(
+    'UTF-8')
 jsonfile = json.loads(str_rh)
+
 for i, symbol in enumerate(jsonfile['results']):
     if symbol and float(symbol['price']) <= 2.00:
         print('%s @ $%s' % (x[i], symbol['price']))
