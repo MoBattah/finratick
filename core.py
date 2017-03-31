dbf03d42d494410a1388847531c4880e76187a28
Mo Battah <mohhandb@hotmail.com>
2017-03-30 21:32:34 -0400

    Update core.py

--- core.py
+++ core.py
@@ -38,10 +38,12 @@ while s < len(x):
         s += 1
         #exceptions are going to keep getting thrown  here due to the txt file being not uniform
     except urllib.HTTPError:
-        s += 2
+        s += 5
         print(urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + x[s]).read().decode(
             'UTF-8'))
-        s += 2
+        s += 5
+    except:
+        break
 
 #filter for $2 price point -- WORKING
 for i, symbol in enumerate(jsonfile['results']):
