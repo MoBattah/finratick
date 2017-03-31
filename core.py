7fc8eca165392e8d4dbfa8fb0c5d8ea99daee1a4
Mo Battah <mohhandb@hotmail.com>
2017-03-30 21:02:02 -0400

    Update core.py

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ import urllib.request as urllib
 import codecs
 import json
 
-# To test against the recent changes, use these 3 lines:   **Should be testing against recent changes** But not
+# To test against the recent changes, use these 3 lines:   **Should be testing against recent changes** But not during build
 # finra = 'http://tsp.finra.org/finra_org/ticksizepilot/TSPilotChanges.txt'
 # col_program = 7
 # col_symbol = 1
@@ -27,16 +27,20 @@ for line in csvfile:
 
 # RobinHood API
 rh = urllib.urlopen(
-    'https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + ",".join(map(str, x)).replace(' ', '+'))
+    "https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=" + ",".join(map(str, x)).replace(' ', '+'))
+s = 1
 
-# Below line works. Now onto creating a line which does this automatically.
+while s < len(x):
+    try:
+        print(urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + x[s]).read().decode(
+        'UTF-8'))
+        #exceptions are going to keep getting thrown  here due to the txt file being not uniform
+    except urllib.HTTPError:
+        s = s + 2
+        print(urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=' + x[s]).read().decode(
+            'UTF-8'))
 
-# This must be done automatically, not manually 
-str_rh = urllib.urlopen('https://api.robinhood.com/prices/?delayed=false&source=nls&symbols=NIHD,FCEL,NAO,AAPL,GSOL').read().decode(
-
-    'UTF-8')
-jsonfile = json.loads(str_rh)
-
+    s = s + 1
 
 #filter for $2 price point -- WORKING
 for i, symbol in enumerate(jsonfile['results']):
