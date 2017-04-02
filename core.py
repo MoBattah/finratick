40751747cf60447e9eab9a778cab91988fbc3b93
Mo Battah <mohhandb@hotmail.com>
2017-04-01 21:09:52 -0400

    Create core.py

---
+++ core.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+import json
+from sys import argv
+
+rh = 'https://api.robinhood.com/prices/?delayed=false&source=nls&symbols='
+
+# process securities list
+tf = list(map(lambda x: x.strip(), open(argv[1]).readlines()))[1:-1]
+tf = list(filter(lambda x: '|G2' in x or '|G3' in x, tf))
+tf = list(map(lambda x: x.split('|')[0], tf))
+
+# get price by ticker
+tickers = list(zip(tf, map(lambda x: x['price'] if x is not None else None, requests.get(rh + ','.join(tf)).json()['results'])))
+tickers = filter(lambda x: x[1] is not None, tickers)
+tickers = sorted(tickers, key=lambda t: float(t[1]), reverse=True)
+
+for ticker in tickers:
+    print(ticker[0] + ': ' + ticker[1])
+
+#Stolen from QLYoung but he will never see this so it does not matter.
